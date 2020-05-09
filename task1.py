96b29ee99dd7a17ecc4961c6fc6a42c5bf5b7cf0
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2020-05-09 11:51:05 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -2,24 +2,47 @@
 
 
 class MyIterator:
-    def __init__(self, start, stop):
-        self.start = start
-        self.stop = stop
+    def __init__(self, *args):
+        self.args = args
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start >= self.stop:
-            raise StopIteration
+        if len(self.args) == 1:
+            a = [i for i in range(self.args[0])]
+            return a
+        elif len(self.args) == 2:
+            a = [i for i in range(self.args[0], self.args[1] - 1)]
+            return a
+        elif len(self.args) == 3:
+            a = [i for i in range(self.args[0] + 1, self.args[1] + 1)][::self.args[2]]
+            return a
         else:
-            self.start += 1
-            return self.start
+            raise Exception('not enough/too much arguments')
 
 
-for i in MyIterator(5, 10):
+m1 = MyIterator(10)
+m2 = MyIterator(-10, 10)
+m3 = MyIterator(-10, 10, -2)
+m4 = MyIterator(-10, 4, 10, -2)
+for i in m1:
     print(i)
+    break
 
+for i in m2:
+    print(i)
+    break
+
+for i in m3:
+    print(i)
+    break
+
+'''
+for i in m4:
+    print(i)
+    break
+'''
 # ---------------------------------
 
 print(list(range(10)))
